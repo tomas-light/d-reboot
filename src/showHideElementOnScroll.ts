34a8b38c06dbaae7fa5ce917e42ec77feabca8af
tomas-light <art.ser.ignatiev@gmail.com>
2025-04-27 20:18:50 +0300

    little improve avatar animation

--- src/showHideElementOnScroll.ts
+++ src/showHideElementOnScroll.ts
@@ -4,7 +4,7 @@ export function showHideElementOnScroll(element: Element) {
   gsap.from(element, {
     width: 0,
     ['margin-left']: '-0.5rem', // когда элемент скрыт, без этого сдвига ощущение, что между текстом два пробела стоит
-    display: 'inline-flex',
+    // display: 'inline-flex',
     scrollTrigger: {
       start: 'top 40%', // анимация запускается, когда группа символов достигает x% скрола сверху экрана
       end: 'top 40%', // анимация завершается, когда группа символов достигает y% скрола сверху экрана
