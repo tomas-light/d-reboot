b22c98d2bef4ed1182475cb1812caef1bc3d7c8b
tomas-light <art.ser.ignatiev@gmail.com>
2025-05-17 21:04:05 +0300

    (4) try to add deploy to github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type UserConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [tailwindcss()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern-compiler',
+export default defineConfig(
+  (env): UserConfig => ({
+    plugins: [tailwindcss()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        },
       },
     },
-  },
-});
+    build: {},
+    base: env.mode === 'production' && !env.isPreview ? '/d-reboot/' : '',
+  })
+);
