accecefee6d3ea34ac3c3493dc871944f5ee129a
tomas-light <art.ser.ignatiev@gmail.com>
2025-05-17 14:23:52 +0300

    add show/hide animation for rive

--- src/rive-animations/attachRivAnimation.ts
+++ src/rive-animations/attachRivAnimation.ts
@@ -1,4 +1,10 @@
-import { Alignment, Fit, Layout, Rive } from '@rive-app/canvas';
+import {
+  Alignment,
+  Fit,
+  Layout,
+  Rive,
+  StateMachineInput,
+} from '@rive-app/canvas';
 import { resizeRiveOnWindowResize } from './resizeRiveOnWindowResize';
 
 // same hardcoded name in all animations (the name is embedded in files)
@@ -8,20 +14,43 @@ export function attachRivAnimation(
   canvas: HTMLCanvasElement,
   rivName: string | undefined
 ) {
-  const riveInstance = new Rive({
-    src: `/rive-animations/${rivName}.riv`,
-    canvas,
-    stateMachines: RIVE_ANIMATIONS_STATE_MACHINE_NAME,
-    layout: new Layout({
-      fit: Fit.FitWidth, // Change to: rive.Fit.Contain, or Cover
-      alignment: Alignment.Center,
-    }),
-    autoplay: true,
-    onLoad: () => {
-      // Prevent a blurry canvas by using the device pixel ratio
-      riveInstance.resizeDrawingSurfaceToCanvas();
-    },
-  });
+  return new Promise<{
+    riveInstance: Rive;
+    stateMachineInputs: StateMachineInput[];
+    playIconShowing: VoidFunction;
+    playIconHiding: VoidFunction;
+  }>((resolve) => {
+    const riveInstance = new Rive({
+      src: `/rive-animations/${rivName}.riv`,
+      canvas,
+      stateMachines: RIVE_ANIMATIONS_STATE_MACHINE_NAME,
+      layout: new Layout({
+        fit: Fit.FitWidth, // Change to: rive.Fit.Contain, or Cover
+        alignment: Alignment.Center,
+      }),
+      // autoplay: true,
+      onLoad: () => {
+        // Prevent a blurry canvas by using the device pixel ratio
+        riveInstance.resizeDrawingSurfaceToCanvas();
 
-  resizeRiveOnWindowResize(riveInstance);
+        const stateMachineInputs = riveInstance.stateMachineInputs(
+          RIVE_ANIMATIONS_STATE_MACHINE_NAME
+        );
+        resolve({
+          riveInstance,
+          stateMachineInputs,
+          playIconShowing: () => {
+            const stateMachine = stateMachineInputs?.[0];
+            stateMachine.value = 0; // embedded behavior
+          },
+          playIconHiding: () => {
+            const stateMachine = stateMachineInputs?.[0];
+            stateMachine.value = 1; // embedded behavior
+          },
+        });
+      },
+    });
+
+    resizeRiveOnWindowResize(riveInstance);
+  });
 }
