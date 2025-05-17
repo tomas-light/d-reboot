e0648bf3924fe01f6b28a82909a832701bc2773e
tomas-light <art.ser.ignatiev@gmail.com>
2025-05-17 20:28:20 +0300

    replace favicon inserting by separated file

--- src/insertFavicon.ts
+++
@@ -1,8 +0,0 @@
-export function insertFavicon(svgHtmlCode: string) {
-  const encodedSVG = svgHtmlCode.replace(/\"/g, '%22').replace(/\#/g, '%23');
-
-  const link = document.createElement('link');
-  link.rel = 'icon';
-  document.head.appendChild(link);
-  link.href = `data:image/svg+xml,${encodedSVG}`;
-}
