99cd1226dc7d3aa67a1edfa70d76e9aaa55d50e0
tomas-light <art.ser.ignatiev@gmail.com>
2025-05-18 17:59:26 +0300

    setup BASE_URL_TO_PUBLIC via github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,28 @@
 import tailwindcss from '@tailwindcss/vite';
 import { writeFile } from 'node:fs/promises';
 import path from 'path';
+import process from 'process';
 import { fileURLToPath } from 'url';
 import { defineConfig, type UserConfig } from 'vite';
 
 export const paths = {
   __dirname: path.dirname(fileURLToPath(import.meta.url)),
-  get generatedEnvFile() {
+  get env() {
     return path.join(this.__dirname, 'src', 'env.json');
   },
 };
 
-export default defineConfig(async (env): Promise<UserConfig> => {
-  const BASE_URL_TO_PUBLIC =
-    env.mode === 'production' && !env.isPreview ? '/d-reboot/' : '/';
+export default defineConfig(async (): Promise<UserConfig> => {
+  const BASE_URL_TO_PUBLIC = process.env?.BASE_URL_TO_PUBLIC ?? '/';
 
-  const generatedEnvContent = JSON.stringify(
+  const envContent = JSON.stringify(
     {
-      BASE_URL_TO_PUBLIC,
+      BASE_URL_TO_PUBLIC: BASE_URL_TO_PUBLIC,
     },
     null,
     2
   );
-
-  await writeFile(paths.generatedEnvFile, generatedEnvContent, 'utf8');
+  await writeFile(paths.env, envContent, 'utf8');
 
   return {
     plugins: [tailwindcss()],
@@ -34,7 +33,6 @@ export default defineConfig(async (env): Promise<UserConfig> => {
         },
       },
     },
-    build: {},
     base: BASE_URL_TO_PUBLIC,
   };
 });
