33e32f9540f68dfcce3b178a8fc7b1ef49c6172d
tomas-light <art.ser.ignatiev@gmail.com>
2025-05-23 18:48:04 +0300

    upgrade scroll trigger to react on window width changes. Animation now starts with very firest scroll movement

--- src/scroll/createScrollTriggerWithAnimations.ts
+++ src/scroll/createScrollTriggerWithAnimations.ts
@@ -1,53 +1,176 @@
 import { gsap } from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
+type ChangeElementState = (element: HTMLElement) => void;
+
 export async function createScrollTriggerWithAnimations(options: {
   elementsToBind: NodeListOf<Element>;
   changeState: {
-    showElement: (element: HTMLElement) => void;
-    hideElement: (element: HTMLElement) => void;
-    showRive: (element: HTMLElement) => void;
-    hideRive: (element: HTMLElement) => void;
+    showElement: ChangeElementState;
+    hideElement: ChangeElementState;
+    showRive: ChangeElementState;
+    hideRive: ChangeElementState;
   };
 }) {
   const { elementsToBind, changeState } = options;
 
   gsap.registerPlugin(ScrollTrigger);
 
-  ScrollTrigger.create({
-    trigger: elementsToBind,
-    start: 'top 40%', // анимация запускается, когда элемент достигает x% скрола сверху экрана
-    scrub: true, // привязка к позиции скрола, а не ко времени
-    end: 'bottom 40%', // анимация завершается, когда последний элемент достигает y% скрола снизу экрана
-    endTrigger: elementsToBind[elementsToBind.length - 1],
-    onUpdate: (scrollTrigger) => {
-      const closestIndex = Math.trunc(
-        scrollTrigger.progress * elementsToBind.length
-      );
+  const { getStateModify, initializeElementsState } =
+    createElementsState(changeState);
+  initializeElementsState(Array.from(elementsToBind) as HTMLElement[]);
 
+  let trigger: ScrollTrigger | undefined = undefined;
+  let lastWidth: number | undefined;
+
+  const resizeObserver = new ResizeObserver((entries) => {
+    window.requestAnimationFrame(() => {
+      if (!Array.isArray(entries) || !entries.length) {
+        return;
+      }
+
+      entries.forEach((entry) => {
+        const [borderBoxSize] = entry.borderBoxSize;
+        const { inlineSize: width } = borderBoxSize;
+        if (width === lastWidth) {
+          return;
+        }
+
+        lastWidth = width;
+
+        const elements = Array.from(elementsToBind) as HTMLElement[];
+        const firstElement = elements[0];
+        const offsetTop = firstElement.offsetTop;
+
+        const start = `top ${offsetTop}`;
+        const end = `bottom ${offsetTop}`;
+
+        if (!trigger) {
+          trigger = ScrollTrigger.create({
+            start,
+            end,
+            trigger: elementsToBind,
+            endTrigger: elementsToBind[elementsToBind.length - 1],
+            scrub: true, // привязка к позиции скрола, а не ко времени
+            onUpdate: (scrollTrigger) => {
+              const closestIndex = Math.trunc(
+                scrollTrigger.progress * elementsToBind.length
+              );
+              updateElementsState(closestIndex);
+            },
+          });
+        } else {
+          trigger.vars.start = start;
+          trigger.vars.end = end;
+          trigger.refresh();
+
+          const closestIndex = Math.trunc(
+            trigger.progress * elementsToBind.length
+          );
+          updateElementsState(closestIndex);
+        }
+      });
+    });
+
+    function updateElementsState(elementIndex: number) {
       const elements = Array.from(elementsToBind) as HTMLElement[];
-      const isScrollDown = scrollTrigger.direction > 0;
-      if (isScrollDown) {
-        // работаем со всеми элементами для случаев, когда скролл резко изменил позицию, чтобы не оставалось дырок
-        const elementsBefore = elements.slice(0, closestIndex + 1);
-        elementsBefore.forEach((element) => {
-          if (element.dataset.riveName != null) {
-            changeState.showRive(element);
-          } else {
-            changeState.showElement(element);
-          }
-        });
-      } else {
-        // работаем со всеми элементами для случаев, когда скролл резко изменил позицию, чтобы не оставалось дырок
-        const elementsAfter = elements.slice(closestIndex);
-        for (const element of elementsAfter) {
-          if (element.dataset.riveName != null) {
-            changeState.hideRive(element);
-          } else {
-            changeState.hideElement(element);
-          }
+
+      for (let index = 0; index < elements.length; index++) {
+        const state = getStateModify(elements, index);
+        if (elementIndex > 0 && index <= elementIndex) {
+          state.show();
+        } else {
+          state.hide();
         }
       }
-    },
+    }
   });
+
+  resizeObserver.observe(document.body);
+}
+
+function createElementsState(changeState: {
+  showElement: ChangeElementState;
+  hideElement: ChangeElementState;
+  showRive: ChangeElementState;
+  hideRive: ChangeElementState;
+}) {
+  const showedElementIndexes = new Set<number>();
+  const hiddenElementIndexes = new Set<number>();
+
+  const showedRiveIndexes = new Set<number>();
+  const hiddenRiveIndexes = new Set<number>();
+
+  return {
+    initializeElementsState,
+    getStateModify,
+  };
+
+  function initializeElementsState(elements: HTMLElement[]) {
+    for (let index = 0; index < elements.length; index++) {
+      const element = elements[index];
+
+      let showedIndexes: Set<number>;
+      let hiddenIndexes: Set<number>;
+
+      if (element.dataset.riveName == null) {
+        showedIndexes = showedElementIndexes;
+        hiddenIndexes = hiddenElementIndexes;
+      } else {
+        showedIndexes = showedRiveIndexes;
+        hiddenIndexes = hiddenRiveIndexes;
+      }
+
+      if (element.dataset.showed == null) {
+        hiddenIndexes.add(index);
+      } else {
+        showedIndexes.add(index);
+      }
+    }
+  }
+
+  function getStateModify(elements: HTMLElement[], elementIndex: number) {
+    if (
+      showedElementIndexes.has(elementIndex) ||
+      hiddenElementIndexes.has(elementIndex)
+    ) {
+      return {
+        show: () => {
+          hiddenElementIndexes.delete(elementIndex);
+
+          if (!showedElementIndexes.has(elementIndex)) {
+            showedElementIndexes.add(elementIndex);
+            changeState.showElement(elements[elementIndex]);
+          }
+        },
+        hide: () => {
+          showedElementIndexes.delete(elementIndex);
+
+          if (!hiddenElementIndexes.has(elementIndex)) {
+            hiddenElementIndexes.add(elementIndex);
+            changeState.hideElement(elements[elementIndex]);
+          }
+        },
+      };
+    }
+
+    return {
+      show: () => {
+        hiddenRiveIndexes.delete(elementIndex);
+
+        if (!showedRiveIndexes.has(elementIndex)) {
+          showedRiveIndexes.add(elementIndex);
+          changeState.showRive(elements[elementIndex]);
+        }
+      },
+      hide: () => {
+        showedRiveIndexes.delete(elementIndex);
+
+        if (!hiddenRiveIndexes.has(elementIndex)) {
+          hiddenRiveIndexes.add(elementIndex);
+          changeState.hideRive(elements[elementIndex]);
+        }
+      },
+    };
+  }
 }
