9783789a82847e1aac2198331ffacf37e0dee4dd
tomas-light <art.ser.ignatiev@gmail.com>
2025-05-31 14:07:27 +0300

    remove extra wrappers after text splitting

--- src/animate-words/splitAnimatedTextByWords.ts
+++ src/animate-words/splitAnimatedTextByWords.ts
@@ -5,8 +5,39 @@ export function splitAnimatedTextByWords(textSelector: string) {
     document.querySelectorAll(textSelector)
   ) as HTMLElement[];
 
-  animatedCharacters.forEach(
+  const splitTypes = animatedCharacters.map(
     (spanElement) =>
       new SplitType(spanElement, { types: 'words', tagName: 'span' })
   );
+
+  removeExtraWrappersThatBreaksLayout(splitTypes);
+}
+
+function removeExtraWrappersThatBreaksLayout(splitTypes: SplitType[]) {
+  for (const splitType of splitTypes) {
+    if (!splitType.words) {
+      continue;
+    }
+
+    for (const wordElement of splitType.words) {
+      wordElement.removeAttribute('style');
+
+      const isNotSpan = wordElement.tagName.toLowerCase() !== 'span';
+      if (isNotSpan) {
+        continue;
+      }
+
+      const { parentElement } = wordElement;
+      if (!parentElement) {
+        continue;
+      }
+
+      const isParentSpan = parentElement.tagName.toLowerCase() === 'span';
+      const hasParentInlineDisplay =
+        parentElement.style.display === 'inline-block';
+      if (isParentSpan && hasParentInlineDisplay) {
+        parentElement.replaceWith(...parentElement.childNodes);
+      }
+    }
+  }
 }
